4aa041ec8ee94e9c3e66c110ad2937446da97729
natalou21 <nathanbeauregard8@gmail.com>
2023-08-30 14:20:25 -0400

    Synchronize mods

---
+++ GenerateArray.go
@@ -0,0 +1,13 @@
+package GenerateArray
+
+import "math/rand"
+
+func ArrayGenerate(size int) []int {
+	var tuple = make([]int, size)
+
+	for i := 0; i < size; i++ {
+		tuple[i] = rand.Intn(10000)
+	}
+
+	return tuple
+}
---
+++ go.mod
@@ -0,0 +1,6 @@
+module GenerateArray
+
+go 1.21.0
+
+require github.com/natalou21/GenerateArray_mod v0.0.0
+replace github.com/natalou21/GenerateArray_mod => ./github.com/natalou21/GenerateArray_mod
